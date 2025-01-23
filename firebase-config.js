baf1178e28c3a30ad7ef40855dd1daac2ec2282a
perasr <p.sriganesh@yahoo.com>
2025-01-23 22:35:59 +0530

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDEDnbKEV396lb_izXVchgF-H5SHfvhtk0",
+    authDomain: "statusupdate-e5f93.firebaseapp.com",
+    databaseURL: "https://statusupdate-e5f93-default-rtdb.firebaseio.com",
+    projectId: "statusupdate-e5f93",
+    storageBucket: "statusupdate-e5f93.firebasestorage.app",
+    messagingSenderId: "1029512636360",
+    appId: "1:1029512636360:web:1e8424e16c25d033cf02fd",
+    measurementId: "G-V9LMLRM3EY"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
